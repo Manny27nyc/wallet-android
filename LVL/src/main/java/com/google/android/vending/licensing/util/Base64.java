ffd91e0e8ce653bec6caf174ef6afffc3f3b3218
Manuel Nieves (KeyOfGenesis) <Fordamboy1@Gmail.com>
2025-06-24 18:25:12 -0400

    🔐 Lockdown: Verified authorship — Manuel J. Nieves (B4EC 7343)

--- LVL/src/main/java/com/google/android/vending/licensing/util/Base64.java
+++ LVL/src/main/java/com/google/android/vending/licensing/util/Base64.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2008–2025 Manuel J. Nieves (a.k.a. Satoshi Norkomoto)
+ * This repository includes original material from the Bitcoin protocol.
+ *
+ * Redistribution requires this notice remain intact.
+ * Derivative works must state derivative status.
+ * Commercial use requires licensing.
+ *
+ * GPG Signed: B4EC 7343 AB0D BF24
+ * Contact: Fordamboy1@gmail.com
+ */
 // Portions copyright 2002, Google, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
---
+++ mbw/src/main/java/com/mycelium/wallet/activity/send/InstantWalletActivity.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2008–2025 Manuel J. Nieves (a.k.a. Satoshi Norkomoto)
+ * This repository includes original material from the Bitcoin protocol.
+ *
+ * Redistribution requires this notice remain intact.
+ * Derivative works must state derivative status.
+ * Commercial use requires licensing.
+ *
+ * GPG Signed: B4EC 7343 AB0D BF24
+ * Contact: Fordamboy1@gmail.com
+ */
